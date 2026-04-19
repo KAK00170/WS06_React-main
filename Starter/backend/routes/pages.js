5f4c2fc7e9d7055d6b7535bd67ffefd65e90f525
Kaapo Kivimäki <kak00170@laurea.fi>
2026-04-19 23:06:55 +0300

    .1

--- Starter/backend/routes/pages.js
+++ Starter/backend/routes/pages.js
@@ -3,4 +3,8 @@ const path = require('path');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.sendFile(path.join(req.app.locals.publicDir, 'index.html'));
+});
+
 module.exports = router;
